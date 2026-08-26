b58d7590b033f5a3f2ffd338ca838b2cfa51ff9e
nobodys1000000000-dev <nobodys1000000000@gmail.com>
2026-08-27 00:37:20 +0100

    Update طلبات.js

--- script/commands/developer/طلبات.js
+++ script/commands/developer/طلبات.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "طلبات",
   version: "1.2.0",
   hasPermssion: 2,
-  credits: "ايمن",
+  credits: "nobody",
   description: "إدارة طلبات المجموعات بشكل مختصر",
   commandCategory: "developer",
   usages: "طلبات",
@@ -19,7 +19,7 @@ module.exports.handleReply = async function({ api, event, handleReply }) {
 
   if (!["قبول", "رفض", "اوافق", "ارفض"].includes(action) || nums.length === 0) return;
 
-  let msg = "⌬ ━━ 𝗞𝗜𝗥𝗔 𝗗𝗘𝗩 ━━ ⌬\n\n";
+  let msg = "⌬ ━━ AKI 𝗗𝗘𝗩 ━━ ⌬\n\n";
   try {
     for (let num of nums) {
       let item = handleReply.listRequest[num - 1];
@@ -52,7 +52,7 @@ module.exports.run = async function({ api, event }) {
 
   if (all.length === 0) return api.sendMessage("⌬ ━━ 𝗞𝗜𝗥𝗔 ━━ ⌬\n\nلا توجد طلبات مجموعات حالياً.", threadID, messageID);
 
-  let msg = `⌬ ━━ 𝗞𝗜𝗥𝗔 𝗥𝗘𝗤𝗨𝗘𝗦𝗧𝗦 ━━ ⌬\n\n`;
+  let msg = `⌬ ━━ AKI 𝗥𝗘𝗤𝗨𝗘𝗦𝗧𝗦 ━━ ⌬\n\n`;
   let listRequest = [];
 
   for (let i = 0; i < all.length; i++) {
